8bc82092b4c3b00ffab82408225c67e9a250414a
cfbraatz <cbraatz@uplandsoftware.com>
2020-08-07 19:23:13 -0400

    Tercer commit

--- tweets/views.py
+++ tweets/views.py
@@ -2,10 +2,24 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 
-
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Home View</h1>")
+    #return HttpResponse("<h1>Home View</h1>")
+    return render(request,"pages/home.html",context={},status=200)
+
+def tweet_list_view(request,*args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift or Java/iOS/Android
+    return json data
+    """
+    qs=Tweet.objects.all()
+    tweets_list=[{"id":x.id,"content":x.content} for x in qs]
+    data={
+        "isUser": False,
+        "response":tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs): #tweet_id debe ser el mismo nombre del id especificado en url 'tweets/<int:tweet_id>'
     """
